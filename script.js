9a17ed1ba6c7e7e207a7ebb496f2201c128c1d94
Anushka Verma <130084364+AnuV1905@users.noreply.github.com>
2024-04-14 10:50:19 +0000

    changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<script type="module">
+
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-analytics.js";
@@ -20,4 +20,3 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-</script>
